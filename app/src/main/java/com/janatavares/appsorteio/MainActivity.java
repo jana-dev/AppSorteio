4123d84a904f7e7d1485ab1d9a79d60e6e42abfa
Janaina Tavares <janixgg@outlook.com>
2024-05-14 22:19:30 -0300

    Projeto criado

---
+++ app/src/main/java/com/janatavares/appsorteio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.janatavares.appsorteio;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void sortearNumero(View view){
+        TextView numeroSorteado = findViewById(R.id.numeroSorteado);
+        int numero = (int)(Math.random()*100 + 1);
+        numeroSorteado.setText(String.valueOf("Número sorteado: " + numero));
+    }
+}
